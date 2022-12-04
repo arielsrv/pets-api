32da124de356cc1827853bc235c1b412cf804733
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-12-04 13:39:39 +0100

    fix

--- src/main/app/model/apps/create/request/create_app_request.go
+++
@@ -1,9 +0,0 @@
-package request
-
-import "github.com/src/main/app/ent/property"
-
-type CreateAppRequest struct {
-	Name      string           `json:"name,omitempty"`
-	GroupID   int64            `json:"group_id,omitempty"`
-	AppTypeID property.AppType `json:"app_type_id,omitempty"`
-}
